720b983c91e2c6a8ae2f4ff0ff338c495e264048
James Brink <brink.james@gmail.com>
2014-04-20 22:07:38 -0700

    Modified postgres.sh to allow graceful shutdown of PostgreSQL service

--- postgres.sh
+++ postgres.sh
@@ -11,6 +11,6 @@ create_user () {
     fi
 }
 create_user &
-/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf
+exec /usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf
 
 
