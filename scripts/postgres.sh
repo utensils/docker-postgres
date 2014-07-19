0aa3799bbe9bb99e502de288b866650f2db32d7b
Zachary Maillard <sagebrushgis@gmail.com>
2014-07-19 10:56:39 -0600

    Cleaned up if statement

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 create_user () {
     if [ -e /var/tmp/firstrun ]; then
-	echo "Waiting for PostgreSQL to start"
-	while [ ! -e /var/run/postgresql/9.3-main.pid ]; do
+    echo "Waiting for PostgreSQL to start"
+    while [ ! -e /var/run/postgresql/9.3-main.pid ]; do
             inotifywait -q -q -e create /var/run/postgresql/
         done
 
-	# We sleep here for 2 seconds to allow clean output, and speration from postgres startup messages
-	sleep 2
+    # We sleep here for 2 seconds to allow clean output, and speration from postgres startup messages
+    sleep 2
         echo "Below are your configured options."
         echo -e "================\nUSER: $USER\nPASSWORD: $PASSWORD\nSCHEMA: $SCHEMA\nPOSTGIS: $POSTGIS\n================"
         if [ $USER == "postgres" ]; then
@@ -19,9 +19,14 @@ create_user () {
             echo "CREATE USER :user WITH SUPERUSER PASSWORD :'password' ;" | psql --set user=$USER --set password=$PASSWORD && createdb $SCHEMA 
         fi
 
-        if $POSTGIS; then
+
+        if [ "$POSTGIS" = "True" ]; then
+            echo "CREATING EXTENSIONS"
             echo "CREATE EXTENSION postgis;CREATE EXTENSION postgis_topology;" | psql -d $SCHEMA
+        else
+            echo "NOT CREATING EXTENSIONS"
         fi
+
         rm /var/tmp/firstrun
     fi
 }
